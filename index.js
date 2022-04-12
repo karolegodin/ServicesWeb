b333e726c64191decd76c5659da723c485afac1c
SarahC <sclaude@enssat.fr>
2022-04-12 15:17:44 +0200

    Ajout du bot Steeve sur Discord

---
+++ index.js
@@ -0,0 +1,89 @@
+// Require the necessary discord.js classes
+const fs = require('node:fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.login(token);
+
+/*
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const trim = (str, max) => (str.length > max ? `${str.slice(0, max - 3)}...` : str);
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'cat') {
+		await interaction.deferReply();
+		const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+
+		interaction.editReply({ files: [file] });
+	} else if (commandName === 'urban') {
+		await interaction.deferReply();
+		const term = interaction.options.getString('term');
+		const query = new URLSearchParams({ term });
+
+		const { list } = await fetch(`https://api.urbandictionary.com/v0/define?${query}`).then(response => response.json());
+
+		if (!list.length) {
+			return interaction.editReply(`No results found for **${term}**.`);
+		}
+
+		const [answer] = list;
+
+		const embed = new MessageEmbed()
+			.setColor('#EFFF00')
+			.setTitle(answer.word)
+			.setURL(answer.permalink)
+			.addFields(
+				{ name: 'Definition', value: trim(answer.definition, 1024) },
+				{ name: 'Example', value: trim(answer.example, 1024) },
+				{ name: 'Rating', value: `${answer.thumbs_up} thumbs up. ${answer.thumbs_down} thumbs down.` },
+			);
+		interaction.editReply({ embeds: [embed] });
+	}
+});
+
+client.login('your-token-goes-here');*/
